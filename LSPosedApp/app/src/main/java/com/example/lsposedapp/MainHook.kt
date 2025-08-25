f79e9ad8ec6158fe0846ce39e0729bb4ac8e8660
mathewgeola <mathewgeola@gmail.com>
2025-08-25 16:25:36 +0800

    LSPosedApp v1

---
+++ LSPosedApp/app/src/main/java/com/example/lsposedapp/MainHook.kt
@@ -0,0 +1,17 @@
+package com.example.lsposedapp
+
+import android.util.Log
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+
+class MainHook : IXposedHookLoadPackage {
+    companion object {
+        private const val TAG = "MainHook"
+    }
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        Log.d(TAG, "handleLoadPackage: lpparam: ${lpparam}")
+    }
+
+}
